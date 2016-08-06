a6efb1f8b2c8579cc845a6a32eb7b54e4b777778
Radhika Gulati <radhigulati@gmail.com>
2016-08-05 23:55:24 -0700

    house robber

---
+++ houserobber.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def rob(self, nums):
+        
+        if len(nums) == 0:
+            return 0
+        elif len(nums) == 1:
+            return nums[0]
+            
+        length = len(nums)
+        work = [nums[0]]
+        work.append(max(nums[0],nums[1]))
+        
+        for i in range(2, length, 1):
+            work.append(max(work[i-2] + nums[i], work[i-1]))
+            
+        return work[len(work)-1]
+
+s = Solution()
+print (s.rob(nums[4,6,13,2]))
